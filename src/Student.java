b9f894d8074dc669049e08e509033ff618f72b2e
ZIN MYO <overmidnight12@gmail.com>
2020-03-25 19:28:51 -0700

    my changes

---
+++ src/Student.java
@@ -0,0 +1,8 @@
+
+public class Student {
+	public String fname = "Su Su";
+	public String lname = "Aung";
+	public String email = "SuSu@gmail.com";
+	public String major = "Computing";
+	public int age = 18;	
+}
